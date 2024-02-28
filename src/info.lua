574983467fe10d6ab3e0d4df509b1114bbae4f49
Moncak <moncak@MonBian.deb.monblog.top>
2024-02-28 22:46:14 +0800

    修复了init.lua生成错误配置文件的问题

--- src/info.lua
+++ src/info.lua
@@ -21,7 +21,8 @@ YourPeerInfo = {
     ASN = "4211110001",
     PrivateKey = "01234567890123456789012345678901234567890123",
     PublicKey = "01234567890123456789012345678901234567890123",
-    IP = "172.16.254.254",  
+    IP = "172.16.254.254",
+    IPSegment = "172.16.254.1/24",
     Endpoint = "example.com",                                    
     Port = "23333",                                             --m
 }
@@ -32,12 +33,15 @@ Confs = {
     Birdconf = "/etc/bird.conf",
     WQOconf = "/etc/wg-quick-op.yaml",
     PUconf = "/etc/wireguard/port_using.log",
+    WGDic = "/etc/wireguard"
 }
 
 --just for test
 --[[
-Confs.WGconf = "wg0.conf"
-Confs.Birdconf = "bird.conf"
-Confs.WQOconf = "wg-quick-op.yaml"
-Confs.PUconf = "port_using.log"
+os.execute("mkdir -p ./testconf")
+Confs.WGconf = "./testconf/wireguard/wg0.conf"
+Confs.Birdconf = "./testconf/bird.conf"
+Confs.WQOconf = "./testconf/wg-quick-op.yaml"
+Confs.PUconf = "./testconf/port_using.log"
+Confs.WGDic = "./testconf/wireguard"
 --]]
